a2dc4bf66c97da8d8f30b4c18c082ee77f4dccfd
Dave <DJSaunders1997@gmail.com>
2021-03-14 14:36:28 +0000

    Added querying current calorie amount

--- Bary-SmartAssistant.py
+++ Bary-SmartAssistant.py
@@ -91,7 +91,6 @@ def add_calories(new_calories):
 
 def remove_calories(minus_calories):
     '''
-    #TODO: change add calories logic
     This function will reduce the total calories stored on sheets if the row exists.
     It will update the spreadsheet if there is already a record with todays date there.
     If the row doesn't exist then create it with negative calories.
@@ -127,6 +126,48 @@ def remove_calories(minus_calories):
         # Delete existing and add new row over
         worksheet.delete_rows(len(data))
         worksheet.insert_row(values, index=len(data), value_input_option='RAW')
+
+    # Inserting any dates turns type into string, thing this will fix this and allow us set all of that column to string
+    fmt = cellFormat(
+        horizontalAlignment='RIGHT',
+        numberFormat=NumberFormat('DATE','yyyy-mm-dd')
+        )
+    format_cell_range(worksheet, 'A2:A', fmt)    
+
+    return(total_calories)    ## Return this so Barry can say how many calories I have left
+
+def query_calories():
+    '''
+    #TODO: change add calories logic
+    This function will return the the calories stored on sheets if the row exists.
+    It will return the number if there is already a record with today's date there.
+    If the row doesn't exist then create it with 0 calories.
+    '''
+
+    # First need to get data as it could have changed inside of the loop
+    data = worksheet.get('A1:B')    # Get all data ignoring the headers
+
+    most_recent_date_str = data[-1][0] # Get lasts item from list / most recent record. First index which is date 
+    most_recent_date_dt = datetime.datetime.strptime(most_recent_date_str, "%Y-%m-%d").date()
+
+    today = datetime.date.today()
+    today_formatted = today.strftime("%Y-%m-%d")
+
+    if most_recent_date_dt < today:
+        # then today's date isnt in the spreadsheet
+        # Add new row
+        print('Adding new row to table')
+        total_calories = 0
+        values = [today_formatted, total_calories]
+        # Adding to the first row that's not full
+        worksheet.insert_row(values, index=len(data)+1, value_input_option='USER_ENTERED')
+
+    elif most_recent_date_dt == today:
+        # Get current calories from the existing record
+        print('Reading from existing row')
+        current_calories = int(data[-1][1].replace(',','')) # Last row, calories item, parsed from string to int
+ 
+        total_calories=current_calories
 
     # Inserting any dates turns type into string, thing this will fix this and allow us set all of that column to string
     fmt = cellFormat(
@@ -187,6 +228,13 @@ def calorie_events(text, total_calories):
             speak('No numbers were said, so I cant add anything sorry!')    
     
     #TODO: Implement a path asking how many calories you have + how many calories remaining.
+    elif ('how many' in text) or ('how much' in text):
+        print('Querying how many calories have been stored')
+        total_calories = query_calories()
+        speak(f'You have consumed {total_calories} calories')
+
+    #IDEA: Maybe I should have a feature asking how many more calories you can consume in a day?
+    # For example if you have consummed 1800 calories then you have 200 left.
 
     else:
         speak('Please specify what calorie action you would like to perform')
