06cc7d1f6c429c7f0953d060e9bbe44f89e441b6
Dave@Pi <DJSaunders1997@gmail.com>
2021-04-22 13:47:25 +0100

    Inital removing sheets and adding Mongo module changes. -- Will not yet run properly

--- Bary-SmartAssistant.py
+++ Bary-SmartAssistant.py
@@ -22,17 +22,8 @@ from google_home_led_pattern import GoogleHomeLedPattern # Local library needs f
 
 pixels.pattern = GoogleHomeLedPattern(show=pixels.show) # Initilise patern to be googles
 
-# Google sheets integration imports
-import gspread
-import datetime
-from gspread_formatting import *
-
-# Connecting to googlesheets
-gc = gspread.service_account(filename='/home/pi/Calorie-Assistant/calorieassistant-SACred.json')
-sh = gc.open('CaloriesSheet') # Open spreadsheet
-worksheet_dave = sh.get_worksheet(0) # First Worksheet Dave Calories 
-worksheet_meg = sh.get_worksheet(1) # Second Worksheet Meg Calories
-
+# Replace GoogleSheets imports with Custom MongoDB Module
+import DBConModule
 
 # Seeing what microphones we have available
 # for index, name in enumerate(sr.Microphone.list_microphone_names()):
@@ -94,143 +85,6 @@ def get_audio():
 
     return said.lower()
 
-# Adding calories logic
-
-def add_calories(new_calories, worksheet):
-    '''
-    This function is used to add rows to the attached google sheets workbook.
-    It will update the spreadsheet if there is already a record with todays date there.
-    Otherwise it will create a new record with today's date
-    '''
-
-    # First need to get data as it could have changed inside of the loop
-    data = worksheet.get('A1:B')    # Get all data ignoring the headers
-
-    most_recent_date_str = data[-1][0] # Get lasts item from list / most recent record. First index which is date 
-    most_recent_date_dt = datetime.datetime.strptime(most_recent_date_str, "%Y-%m-%d").date()
-
-    today = datetime.date.today()
-    today_formatted = today.strftime("%Y-%m-%d")
-
-    if most_recent_date_dt < today:
-        # then today's date isnt in the spreadsheet
-        # Add new row
-        print('Adding new row to table')
-        total_calories = new_calories
-        values = [today_formatted, total_calories]
-        # Adding to the first row that's not full
-        worksheet.insert_row(values, index=len(data)+1, value_input_option='USER_ENTERED')
-
-    elif most_recent_date_dt == today:
-        # Get current calories from the existing record
-        # Add my new calories to the total
-        print('Updating existing row')
-        current_calories = int(data[-1][1].replace(',','')) # Last row, calories item, parsed from string to int
-        total_calories = current_calories + new_calories
-        values = [today_formatted, total_calories]
-        # Delete existing and add new row over
-        worksheet.delete_rows(len(data))
-        worksheet.insert_row(values, index=len(data), value_input_option='RAW')
-
-    # Inserting any dates turns type into string, thing this will fix this and allow us set all of that column to string
-    fmt = cellFormat(
-        horizontalAlignment='RIGHT',
-        numberFormat=NumberFormat('DATE','yyyy-mm-dd')
-        )
-    format_cell_range(worksheet, 'A2:A', fmt)    
-
-    return(total_calories)    ## Return this so Barry can say how many calories I have left
-
-def remove_calories(minus_calories, worksheet):
-    '''
-    This function will reduce the total calories stored on sheets if the row exists.
-    It will update the spreadsheet if there is already a record with todays date there.
-    If the row doesn't exist then create it with negative calories.
-    '''
-
-    minus_calories = -minus_calories # Convert positive into negative
-
-    # First need to get data as it could have changed inside of the loop
-    data = worksheet.get('A1:B')    # Get all data ignoring the headers
-
-    most_recent_date_str = data[-1][0] # Get lasts item from list / most recent record. First index which is date 
-    most_recent_date_dt = datetime.datetime.strptime(most_recent_date_str, "%Y-%m-%d").date()
-
-    today = datetime.date.today()
-    today_formatted = today.strftime("%Y-%m-%d")
-
-    if most_recent_date_dt < today:
-        # then today's date isnt in the spreadsheet
-        # Add new row
-        print('Adding new row to table')
-        total_calories = minus_calories
-        values = [today_formatted, total_calories]
-        # Adding to the first row that's not full
-        worksheet.insert_row(values, index=len(data)+1, value_input_option='USER_ENTERED')
-
-    elif most_recent_date_dt == today:
-        # Get current calories from the existing record
-        # Add my new calories to the total
-        print('Updating existing row')
-        current_calories = int(data[-1][1].replace(',','')) # Last row, calories item, parsed from string to int
-        total_calories = current_calories + minus_calories
-        values = [today_formatted, total_calories]
-        # Delete existing and add new row over
-        worksheet.delete_rows(len(data))
-        worksheet.insert_row(values, index=len(data), value_input_option='RAW')
-
-    # Inserting any dates turns type into string, thing this will fix this and allow us set all of that column to string
-    fmt = cellFormat(
-        horizontalAlignment='RIGHT',
-        numberFormat=NumberFormat('DATE','yyyy-mm-dd')
-        )
-    format_cell_range(worksheet, 'A2:A', fmt)    
-
-    return(total_calories)    ## Return this so Barry can say how many calories I have left
-
-def query_calories(worksheet):
-    '''
-    #TODO: change add calories logic
-    This function will return the the calories stored on sheets if the row exists.
-    It will return the number if there is already a record with today's date there.
-    If the row doesn't exist then create it with 0 calories.
-    '''
-
-    # First need to get data as it could have changed inside of the loop
-    data = worksheet.get('A1:B')    # Get all data ignoring the headers
-
-    most_recent_date_str = data[-1][0] # Get lasts item from list / most recent record. First index which is date 
-    most_recent_date_dt = datetime.datetime.strptime(most_recent_date_str, "%Y-%m-%d").date()
-
-    today = datetime.date.today()
-    today_formatted = today.strftime("%Y-%m-%d")
-
-    if most_recent_date_dt < today:
-        # then today's date isnt in the spreadsheet
-        # Add new row
-        print('Adding new row to table')
-        total_calories = 0
-        values = [today_formatted, total_calories]
-        # Adding to the first row that's not full
-        worksheet.insert_row(values, index=len(data)+1, value_input_option='USER_ENTERED')
-
-    elif most_recent_date_dt == today:
-        # Get current calories from the existing record
-        print('Reading from existing row')
-        current_calories = int(data[-1][1].replace(',','')) # Last row, calories item, parsed from string to int
- 
-        total_calories=current_calories
-
-    # Inserting any dates turns type into string, thing this will fix this and allow us set all of that column to string
-    fmt = cellFormat(
-        horizontalAlignment='RIGHT',
-        numberFormat=NumberFormat('DATE','yyyy-mm-dd')
-        )
-    format_cell_range(worksheet, 'A2:A', fmt)    
-
-    return(total_calories)    ## Return this so Barry can say how many calories I have left
-
-
 def calorie_events(text):
     """
     Function is called when the assistant detects the word 'Calories' in the command.
@@ -239,14 +93,12 @@ def calorie_events(text):
 
     if ('megan' in text) or ('meg' in text):
         person = 'Meg'
-        worksheet = worksheet_meg
     else:
         person = 'Dave'
-        worksheet = worksheet_dave
 
     if ('how many' in text) or ('how much' in text):
         print('Querying how many calories have been stored')
-        total_calories = query_calories(worksheet)
+        total_calories = DBConModule.get_daily_total(person)
         speak(f'{person} has {total_calories} calories logged')
 
     elif ('add' in text) or ('ad' in text) or ('at' in text):
@@ -259,9 +111,11 @@ def calorie_events(text):
             calorie_amount = reg_ex.group(0) # If there are multiple numbers then add the first one            
             speak(f'Adding {calorie_amount} calories')            
 
-            # Adding Cloud integration
-            cloudCalories = add_calories(int(calorie_amount), worksheet)
-            speak(f'{person} has {cloudCalories} calories logged')
+            # Cloud integration
+            DBConModule.add_calories(person, int(calorie_amount))
+            total_calories = DBConModule.get_daily_total(person)
+
+            speak(f'{person} has {total_calories} calories logged')
 
         # If no digits were found
         else:
@@ -278,9 +132,11 @@ def calorie_events(text):
             calorie_amount = reg_ex.group(0) # If there are multiple numbers then add the first one            
             speak(f'Taking away {calorie_amount} calories')
 
-            # Cloud integration
-            cloudCalories = remove_calories(int(calorie_amount), worksheet)
-            speak(f'{person} has {cloudCalories} calories logged')
+            # Adding Cloud integration
+            DBConModule.add_calories(person, -int(calorie_amount))
+            total_calories = DBConModule.get_daily_total(person)
+
+            speak(f'{person} has {total_calories} calories logged')
 
         # If no digits were found
         else:
